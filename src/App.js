f82f586424ca63d4269f6162ef6e85c8d41699da
Ciprian <cozma_ciprian19@yahoo.com>
2021-02-11 19:35:23 +0200

    added a route comp

--- src/App.js
+++ src/App.js
@@ -1,49 +1,67 @@
 import React, { useState } from "react";
-// import Accordion from "./components/Accordion";
-// import Search from './components/Search';
-// import Dropdown from './components/Dropdown';
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
 
 const App = () => {
-
   const items = [
     {
       title: "What is React?",
-      content: "React is a frontend JS framework!"
+      content: "React is a frontend JS framework!",
     },
     {
       title: "Why use React?",
-      content: "React is a favourite JS libray among engineers."
+      content: "React is a favourite JS libray among engineers.",
     },
     {
       title: "How do you use React?",
-      content: "You use React by creating components."
-    }
-  ]
+      content: "You use React by creating components.",
+    },
+  ];
 
   const options = [
     {
       label: "Red",
-      value: "red"
+      value: "red",
     },
     {
       label: "Green",
-      value: "green"
+      value: "green",
     },
     {
       label: "Blue",
-      value: "blue"
-    }
-  ]
+      value: "blue",
+    },
+  ];
 
   const [selected, setSelected] = useState(options[0]);
 
-  return <div className="ui styled accordion">
-    {/* <Accordion items={items}/> */}
-    {/* <Search /> */}
-    {/* <Dropdown options={options} selected={selected} onSelectedChange={setSelected}/> label={"Select a color: "} */}
-    <Translate />
-  </div>;
+  return (
+    <div className="ui styled accordion">
+      {/* <Accordion items={items}/> */}
+      {/* <Search /> */}
+      {/* <Dropdown options={options} selected={selected} onSelectedChange={setSelected}/> label={"Select a color: "} */}
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/search">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+          label={"Select a color: "}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
 };
 
 export default App;
